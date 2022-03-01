97378a3d35cdd71ae8032daa8f2dd1e9e37685e2
Gabriele <gabriele.nava@iit.it>
2022-03-01 19:59:42 +0100

    controller working with all old demos

--- software/aerodynamic-flight-controllers/momentum-based-flight-aerodynamics/src/generate_wind/generate_wind.m
+++ software/aerodynamic-flight-controllers/momentum-based-flight-aerodynamics/src/generate_wind/generate_wind.m
@@ -3,21 +3,43 @@
 
 function v_wind = generate_wind(t, Config)
 
-v_const = 3 * Config.constant_wind;  % constant wind speed [m/s]
-v_gust  = 10;                        % wind gust peak value [m/s]
-v_raise = v_gust - v_const;          % difference between wind gust and constant wind speed
-sigma   = 2;                         % sigma for gaussian distribution
-mu      = 8;                         % mean value for gaussian distribution
+v_const        = Config.constant_wind; % constant wind speed [m/s]
+wind_direction = Config.wind_direction;
+v_ramp_max     = Config.wind_gust_ramp;
+t_init_ramp    = Config.t_init_wind_ramp;
+t_slope_ramp   = Config.t_slope_wind_ramp;
+t_plateau_ramp = Config.t_plateau_wind_ramp;
+v_cosine_max   = Config.wind_gust_cosine;
+t_init_cosine  = Config.t_init_wind_cosine;
+t_delta_cosine = Config.delta_t_wind_cosine;
 
-% generate gaussian distributed wind along time to simulate the wind gust
-if Config.wind_gust
+% parametrized ramp profile
+if t > t_init_ramp && t <= (t_init_ramp + t_slope_ramp)
     
-    if t <= 20
-        v_wind = [-(v_const+v_raise*exp(-(t - mu).^2/(2*sigma^2))); 0; 0];
-    else
-        v_wind = [-v_const; 0; 0];
-    end
+    v_ramp = v_ramp_max * ((t-t_init_ramp)/t_slope_ramp);
+    
+elseif t > (t_init_ramp + t_slope_ramp) && t <= (t_init_ramp + t_slope_ramp + t_plateau_ramp)
+    
+    v_ramp = v_ramp_max;
+    
+elseif t > (t_init_ramp + t_slope_ramp + t_plateau_ramp) && t <= (t_init_ramp + 2*t_slope_ramp + t_plateau_ramp)
+    
+    v_ramp = v_ramp_max * (1-(t-(t_init_ramp + t_slope_ramp + t_plateau_ramp))/t_slope_ramp);
+    
 else
-    v_wind = [-v_const; 0; 0]; % constant wind
+    v_ramp = 0;
 end
+
+% parametrized cosine profile
+if t > t_init_cosine && t <= (t_init_cosine + t_delta_cosine)
+    
+    v_cosine = 0.5*v_cosine_max*(1-cos((t-t_init_cosine)/t_delta_cosine*2*pi));
+    
+else
+    v_cosine = 0;
+end
+
+% total wind profile
+v_wind = (v_const + v_ramp + v_cosine) * wind_direction;
+
 end
