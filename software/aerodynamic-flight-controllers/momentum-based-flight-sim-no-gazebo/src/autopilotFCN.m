18d20ca17abb859a262b02feece44157b2aebc7f
Antonello Paolino <antonello.paolino@iit.it>
2023-11-10 15:47:27 +0100

    Define first Science paper trajectory in autopilot

--- software/aerodynamic-flight-controllers/momentum-based-flight-sim-no-gazebo/src/autopilotFCN.m
+++ software/aerodynamic-flight-controllers/momentum-based-flight-sim-no-gazebo/src/autopilotFCN.m
@@ -1,8 +1,8 @@
-function [joyButtons,joyAxes] = autopilotFCN(time)
+function [joyButtons,joyAxes] = autopilotFCN(time, Config)
     
     persistent front_pressed pitch_c_pressed takeoff_pressed ...
                pitch_ac_pressed yaw_c_pressed yaw_ac_pressed  ...
-               up_pressed down_pressed 
+               up_pressed down_pressed turbo_mode 
     
     front_pressed    = 0;
     pitch_c_pressed  = 0;
@@ -12,31 +12,55 @@ function [joyButtons,joyAxes] = autopilotFCN(time)
     takeoff_pressed  = 0;
     down_pressed     = 0;
     up_pressed       = 0;
+    turbo_mode       = 0;
     
     joyAxes    = zeros(6,1);
     joyButtons = zeros(24,1);
     
+    % Perform In-line trajectory
+    if Config.IN_LINE_TRAJECTORY
+        
+        if time > 10 && time < 17
+            up_pressed = 1;         % Climb up
+        end
+        if time > 20 && time < 30
+            front_pressed = -1;     % Move forward
+            turbo_mode    = 1;      % Move max speed
+        end
+        if time > 38 && time < 39
+            pitch_ac_pressed    = 1;% Bring chest straight
+        end
+        if time > 40 && time < 47
+            down_pressed = 1;       % Approach the ground
+        end
+    
+    % Perform Closed-loop trajectory
+    elseif Config.CLOSED_LOOP_TRAJECTORY
+
+    end
+
+    
     % if button pressed, set to 1
-    if time > 1 && time < 5
-        
-        takeoff_pressed = 1;
-    end
-    if time > 5 && time < 10
-        
-        up_pressed = 1;   
-    end
-    if time > 25 && time < 28
-        
-        down_pressed = 1;   
-    end  
-    if time > 29 && time < 31
-        
-        yaw_c_pressed = 1;
-    end
-    if time > 33 && time < 40
-        
-        front_pressed = -1;
-    end
+    % if time > 1 && time < 5
+    % 
+    %     takeoff_pressed = 1;
+    % end
+    % if time > 5 && time < 10
+    % 
+    %     up_pressed = 1;   
+    % end
+    % if time > 25 && time < 28
+    % 
+    %     down_pressed = 1;   
+    % end  
+    % if time > 29 && time < 31
+    % 
+    %     yaw_c_pressed = 1;
+    % end
+    % if time > 33 && time < 40
+    % 
+    %     front_pressed = -1;
+    % end
 
 %     if time > 25 && time < 28
 %         
@@ -54,15 +78,16 @@ function [joyButtons,joyAxes] = autopilotFCN(time)
     if front_pressed
         joyAxes(2) = front_pressed;
     end
-%     if pitch_c_pressed
-%         joyAxes(6) = pitch_c_pressed;
-%     else
-%         joyAxes(6) = pitch_ac_pressed;
-%     end
+    if pitch_c_pressed
+        joyAxes(6) = pitch_c_pressed;
+    else
+        joyAxes(6) = pitch_ac_pressed;
+    end
         
     joyButtons(13) = up_pressed;
     joyButtons(6)  = takeoff_pressed;
     joyButtons(16) = yaw_ac_pressed;
     joyButtons(15) = yaw_c_pressed;
     joyButtons(14) = down_pressed; 
+    joyButtons(7)  = turbo_mode; 
 end
